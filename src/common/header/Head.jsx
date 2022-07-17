eab7b45dd01c043231b248b5a6fe7aad408f5904
RafiqSultan <Rafiqaasultan@gmail.com>
2022-07-17 13:01:53 -0700

    ADD header into project

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export const Head = () => {
+  return (
+    <>
+    <section className='head'>
+        <div className='container d_flex'>
+            <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label>(+967)777-350-652</label>
+            <i className='fa fa-envelope'></i>
+            <label>Rafiq.alsultan@gmail.com</label>
+
+            </div>
+        
+        <div className='right row RText'>
+        <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <label>EN</label>
+            <label>Ar</label>
+        </div>
+    </div>
+        
+    </section>
+    
+    
+    
+    </>
+  )
+}
