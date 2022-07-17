33ec69a2513b19335aee4f9ef1efd5dff7c2f19f
RafiqSultan <Rafiqaasultan@gmail.com>
2022-07-17 13:39:42 -0700

    ADD sidebar category

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import "./common/header/Header.css";
 import"./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from "./common/header/Header";
+import Pages from "./Pages/Pages";
 
 function App() {
   return (
     <>
    <Router>
         <Header/>
+        <Pages />
         <Routes>
+        
           <Route path='/' exact>
+          
           </Route>
+          
           
         </Routes>
         
