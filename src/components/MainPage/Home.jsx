d35825167bc2991ce7ad5b90bc7153236b27f862
RafiqSultan <Rafiqaasultan@gmail.com>
2022-07-17 14:08:57 -0700

    ADD sidebar home carousel

--- src/components/MainPage/Home.jsx
+++ src/components/MainPage/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Categories from './Categories'
+import "./Home.css"
+import Slider from './Slider'
 
  const Home = () => {
   return (
       <>
-      <Categories/>
+      <section className='home'>
+        <div className='container d_flex'>
+          <Categories />
+          <Slider />
+        </div>
+      </section>
       </>
     
   )
